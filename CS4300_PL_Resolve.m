97c3731e0d3ae16e633f9d2a446ccc7eaf8edca3
rajulr <rajulr@eng.utah.edu>
2016-10-04 19:14:30 -0600

    fixed resolvents changed to array.resolvent structures

--- CS4300_PL_Resolve.m
+++ CS4300_PL_Resolve.m
@@ -1,6 +1,6 @@
 function resolvents = CS4300_PL_Resolve(clause1,clause2)
 
-    resolvents = {};
+    resolvents = [];
     
     num_resolvent = 1;
        
@@ -12,20 +12,21 @@ function resolvents = CS4300_PL_Resolve(clause1,clause2)
                 v1 = [clause1(1:c1-1), clause1(c1+1:end),clause2(1:c2-1), clause2(c2+1:end)]; 
                 %v1 = Rem_Contradictions(v1);
                 v1 = unique(v1);
-                resolvents{num_resolvent} = sort(v1);
+                resolvents(num_resolvent).resolvent = sort(v1);
                 num_resolvent = num_resolvent + 1;
             end
         end
     end
     
     resolvents = Rem_Duplicates(resolvents)
-    
+    %resolvents = unique(resolvents, 'rows');
     
 
 end
 
 function rem_contradictions = Rem_Contradictions(v1)
     rem_contradictions = [];
+    
     rem_counter = 1;
     for i = 1:length(v1)
         for j = i+1:length(v1)
@@ -47,7 +48,7 @@ end
 
 function rem_duplicates = Rem_Duplicates(resolvents)
     
-    rem_duplicates = {};
+    rem_duplicates = [];
     rem_counter = 1;
     if(length(resolvents)==1)
         rem_duplicates = resolvents;
@@ -55,18 +56,18 @@ function rem_duplicates = Rem_Duplicates(resolvents)
     end
     
     for i = 1:length(resolvents)
-        d1 = resolvents{i};
+        d1 = resolvents(i).resolvent;
         for j = i+1:length(resolvents)
-            d2 = resolvents{j};
+            d2 = resolvents(j).resolvent;
             if(isequal(d1, d2))
-                resolvents{j} = [0,0];
+                resolvents(j).resolvent = [0,0];
             end
         end
     end
     
     for i = 1:length(resolvents)
-        if(~isequal(resolvents{i}, [0,0]))
-            rem_duplicates{1, rem_counter} = resolvents{i};
+        if(~isequal(resolvents(i).resolvent, [0,0]))
+            rem_duplicates(rem_counter).resolvent = resolvents(i).resolvent;
             rem_counter = rem_counter + 1;
         end
     end
